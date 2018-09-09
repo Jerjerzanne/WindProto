50d78ece2da0b89ab511bdab7f70eee2ef9c228a
Simon <simon.roy5219@gmail.com>
2018-09-08 22:43:52 -0400

    fixing the camera script

--- Assets/Scripts/FPSCamera.cs
+++ Assets/Scripts/FPSCamera.cs
@@ -2,51 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSCamera : MonoBehaviour {
+public class FPSCamera : MonoBehaviour
+{
 
     public float horizontalSpeed = 2.0F;
     public float verticalSpeed = 2.0F;
-    public float upperAngle = 45.0F;
-    public float lowerAngle = 315.0F;
+    public float lowerAngle = 45.0F;
+    public float upperAngle = 315.0F;
+    private GameObject camera;
 
-    private Transform transform;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        transform = this.GetComponent<Transform>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
-	
-	// Update is called once per 
-    void Update() {
 
-        Debug.DrawRay(transform.position, Vector3.forward, Color.green);
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(camera.transform.position, camera.transform.forward, Color.green);
 
+        float horizontalShift = horizontalSpeed * Input.GetAxis("Mouse X");
+        float verticalShift = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, horizontalShift, 0);
+        float xAngle = camera.transform.eulerAngles.x;
+        float predictedRotation = 0;
+        if (verticalShift > 0 && xAngle >= upperAngle) // moving up while looking up
+        {
+            Debug.Log("red");
+            predictedRotation = xAngle - verticalShift;
+            Debug.Log(predictedRotation);
+            if (predictedRotation <= upperAngle)
+            {
+                camera.transform.Rotate(upperAngle - xAngle, 0, 0);
+            }
+            else
+            {
+                Debug.Log(predictedRotation + " > " + upperAngle);
+                camera.transform.Rotate(-verticalShift, 0 , 0);
+                Debug.Log(camera.transform.eulerAngles);
+            }
+        }
+        else if (verticalShift > 0 && xAngle <= lowerAngle)  // moving up while looking down
+        {
+            Debug.Log("green");
+            predictedRotation = xAngle - verticalShift;
+            if (predictedRotation < 0 && predictedRotation <= (upperAngle - 360.0f))
+            {
+                camera.transform.Rotate(xAngle - upperAngle, 0, 0);
+            }
+            else
+            {
+                camera.transform.Rotate(-verticalShift, 0, 0);
+            }
+        }
+        else if (verticalShift < 0 && xAngle >= upperAngle) // moving down while looking up
+        {
+            Debug.Log("yellow");
+            predictedRotation = xAngle - verticalShift;
+            if (predictedRotation > 360 && (predictedRotation % 360 > lowerAngle))
+            {
+                camera.transform.Rotate(lowerAngle- xAngle, 0, 0);
+            }
+            else
+            {
+                camera.transform.Rotate(-verticalShift, 0, 0);
+            }
+        }
+        else if (verticalShift < 0 && xAngle <= lowerAngle) // moving down while looking down
+        {
+            Debug.Log("Blue");
+            predictedRotation = xAngle - verticalShift;
+            if (predictedRotation >= lowerAngle)
+            {
+                camera.transform.Rotate(lowerAngle - xAngle - 0.01f, 0, 0);
+            }
+            else
+            {
+                camera.transform.Rotate(-verticalShift, 0, 0);
+            }
+        }
+
+       
+    }
+
+    void OnGUI()
+    {
         float h = horizontalSpeed * Input.GetAxis("Mouse X");
         float v = verticalSpeed * Input.GetAxis("Mouse Y");
-
-        transform.Rotate(0, h, 0);
-        GameObject.FindGameObjectWithTag("MainCamera").transform.Rotate(-v,0,0);
-        float zee = GameObject.FindGameObjectWithTag("MainCamera").transform.eulerAngles.z;
-        float xee = GameObject.FindGameObjectWithTag("MainCamera").transform.eulerAngles.x;
-        //Debug.Log(xee);
-        float boi;
-        if (xee > upperAngle && xee < 180)
-        {
-            boi = xee - 45;
-            GameObject.FindGameObjectWithTag("MainCamera").transform.Rotate(-boi, 0, -zee);
-        }
-        else if (xee < lowerAngle && xee > 180)
-        {
-            boi = 315-xee;
-            GameObject.FindGameObjectWithTag("MainCamera").transform.Rotate(boi, 0, -zee);
-            transform.Rotate(boi, 0, 0);
-        }
-        else {
-            GameObject.FindGameObjectWithTag("MainCamera").transform.Rotate(0, 0, -zee);
+        GUI.Button(new Rect(10, 100, 200, 50), "zAngle: " + camera.transform.eulerAngles.z);
+        GUI.Button(new Rect(10, 150, 200, 50), "xAngle: " + camera.transform.eulerAngles.x);
+        GUI.Button(new Rect(10, 200, 100, 50), "h: " + h);
+        GUI.Button(new Rect(10, 250, 100, 50), "v: " + v);
     }
-    }
-            
-	
 }
+
+//Debug.DrawRay(camera.transform.position, camera.transform.forward, Color.green);
+
+//float horizontalShift = horizontalSpeed * Input.GetAxis("Mouse X");
+//float verticalShift = verticalSpeed * Input.GetAxis("Mouse Y");
+//transform.Rotate(0, horizontalShift, 0);
+
+//camera.transform.Rotate(-verticalShift, 0, 0);
+//float xAngle = camera.transform.eulerAngles.x;
+//Debug.Log(verticalShift+ xAngle);
+//float fixAngle;
+//if (xAngle > upperAngle && xAngle< 90)
+//{
+//fixAngle = xAngle - 45;
+//camera.transform.Rotate(-fixAngle, 0, 0);
+//}
+//else if (xAngle<lowerAngle && xAngle> 180)
+//{
+//fixAngle = 315 - xAngle;
+//camera.transform.Rotate(fixAngle, 0, 0);
+//}
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,24 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
+
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+    private bool flightMode = false;
+
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
+            flightMode = false;
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
+            {
                 moveDirection.y = jumpSpeed;
-
+            }
         }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+
+        //if (!controller.isGrounded)
+        //{
+        //    if (Input.GetKeyDown("E"))
+        //    {
+        //        flightMode = !flightMode;
+        //    }
+           
+        //    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        //    moveDirection = transform.TransformDirection(moveDirection);
+        //    moveDirection *= speed;
+        //    if (Input.GetButton("Jump"))
+        //    {
+        //        moveDirection.y = jumpSpeed;
+        //    }
+        //}
+
+        //if (flightMode)
+        //{
+        //    moveDirection.y -= 0.5f * gravity * Time.deltaTime;
+        //    controller.Move(moveDirection * Time.deltaTime);
+        //}
+        //else
+        //{
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Button(new Rect(10, 10, 150, 100), "flightMode: "+flightMode);
     }
 }
 
